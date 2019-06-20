126caa18c173656464e1cf52c5b179b9f1388ab3
shookie <shookiejovial@gmail.com>
2019-06-20 12:34:19 +0900

    spring ioc placeholder

---
+++ src/main/java/kr/or/ddit/ioc/placeholder/DbInfo.java
@@ -0,0 +1,35 @@
+package kr.or.ddit.ioc.placeholder;
+
+public class DbInfo {
+	
+	private String driver;
+	private String url;
+	private String username;
+	private String password;
+	
+	// 생성자 주입
+	public DbInfo(String driver, String url, String username, String password) {
+		this.driver = driver;
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
+}
